a66a33f6c47344716a61d7987916953ef24b1023
kntWT <kento.wata0628@gmail.com>
2024-12-30 18:36:51 +0900

    feat(db): full text index

--- db/init/1_create_table.sql
+++ db/init/1_create_table.sql
@@ -3,11 +3,13 @@ CREATE DATABASE IF NOT EXISTS slack_ask_bot_db;
 USE slack_ask_bot_db;
 
 CREATE TABLE IF NOT EXISTS questions (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  channnel_id VARCHAR(255) NOT NULL,
-  thread_ts VARCHAR(255) NOT NULL,
-  question VARCHAR(255) NOT NULL,
-  user_id VARCHAR(255) NOT NULL,
-  tags VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    channnel_id VARCHAR(255) NOT NULL,
+    thread_ts VARCHAR(255) NOT NULL,
+    question VARCHAR(255) NOT NULL,
+    user_id VARCHAR(255) NOT NULL,
+    tags VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FULLTEXT question_index (question) WITH PARSER ngram,
+    FULLTEXT tags_index (tags)
 );
