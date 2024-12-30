2007412eff6e27858a36753f45f38663795aa0a0
kntWT <kento.wata0628@gmail.com>
2024-12-30 21:57:36 +0900

    fix(api): generatorやめた

--- api/db.py
+++ api/db.py
@@ -1,9 +1,7 @@
 from pydantic import BaseModel
-from sqlalchemy import Column, Integer, String, DateTime, Index, text
+from sqlalchemy import create_engine, MetaData, Table, Column, text
+from sqlalchemy.sql.sqltypes import Integer, String, DateTime
 import datetime
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
 import time
 
 from env import DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
@@ -18,35 +16,29 @@ def connect_db(trial: int):
     try:
         engine = create_engine(
             f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?charset=utf8mb4')
-        SessionLocal = sessionmaker(bind=engine)
-        Base = declarative_base()
-        return engine, SessionLocal, Base
+        meta = MetaData()
+        conn = engine.connect()
+        return engine, meta, conn
     except Exception as e:
         time.sleep(1)
         print(e)
         return connect_db(trial + 1)
 
 
-engine, SessionLocal, Base = connect_db(0)
+engine, meta, conn = connect_db(0)
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-class Question(Base):
-    __tablename__ = "questions"
-    id = Column(Integer, primary_key=True, index=True)
-    channel_id = Column(String)
-    user_id = Column(String)
-    question = Column(String)
-    thread_ts = Column(String)
-    tags = Column(String)
-    created_at = Column(DateTime, default=datetime.datetime.now(JST))
+questions_table = Table(
+    'questions', meta,
+    Column('id', Integer, primary_key=True),
+    Column('channel_id', String(255)),
+    Column('user_id', String(255)),
+    Column('question', String(255)),
+    Column('thread_ts', String(255)),
+    Column('tags', String(255)),
+    Column('created_at', DateTime, default=datetime.datetime.now(JST))
+)
+meta.create_all(engine)
 
 
 class QuestionCreate(BaseModel):
@@ -57,17 +49,18 @@ class QuestionCreate(BaseModel):
     tags: str
 
 
-def create_question(question: QuestionCreate, db: Session = get_db()):
-    db.add(Question(**question.dict()))
-    db.commit()
-    db.refresh(question)
+def create_question(question: QuestionCreate):
+    conn.execute(questions_table.insert().values(
+        **question
+    ))
+    conn.commit()
     return question
 
 
 RELEVANCE_THRESHOLD = 0.5
 
 
-def get_question_by_tags(tags: str, limit: int = 5, db: Session = get_db()):
+def get_question_by_tags(tags: str, limit: int = 5):
     sql = text("""
                SELECT *, MATCH(tags) AGAINST(:tags IN BOOLEAN MODE WITH QUERY EXPANSION) as relevance
                FROM questions
@@ -75,11 +68,11 @@ def get_question_by_tags(tags: str, limit: int = 5, db: Session = get_db()):
                ORDER BY  DESC
                LIMIT :limit
                """)
-    result = db.execute(sql, {"tags": tags, "limit": limit})
+    result = conn.execute(sql, {"tags": tags, "limit": limit})
     return result.fetchall()
 
 
-def get_question_by_question(question: str, limit: int = 5, db: Session = get_db()):
+def get_question_by_question(question: str, limit: int = 5):
     sql = text("""
                SELECT *, MATCH(question) AGAINST(:question IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION) as relevance
                FROM questions
@@ -87,5 +80,5 @@ def get_question_by_question(question: str, limit: int = 5, db: Session = get_db
                ORDER BY  DESC
                LIMIT :limit
                """)
-    result = db.execute(sql, {"question": question, "limit": limit})
+    result = conn.execute(sql, {"question": question, "limit": limit})
     return result.fetchall()
