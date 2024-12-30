266acc987457c9da74af087d3ed99daa5c0624c6
kntWT <kento.wata0628@gmail.com>
2024-12-30 20:23:14 +0900

    fix(api): boltの起動方法

--- api/main.py
+++ api/main.py
@@ -4,15 +4,16 @@ from waitress import serve
 from app import app
 
 # Flaskサーバーの初期化
-flask_app = Flask(__name__)
-handler = SlackRequestHandler(app)
+# flask_app = Flask(__name__)
+# handler = SlackRequestHandler(app)
 
 
-@flask_app.route("/", methods=["POST"])
-def slack_events():
-    return handler.handle(request)
+# @flask_app.route("/", methods=["POST"])
+# def slack_events():
+#     return handler.handle(request)
 
 
 if __name__ == "__main__":
     # 5000番ポートでリッスン
-    serve(flask_app, host="0.0.0.0", port=5000, threads=6)
+    # serve(flask_app, host="0.0.0.0", port=5000, threads=6)
+    app.start(port=5000)
